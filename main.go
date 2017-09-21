10a37e3f157b471b07c99bcfb22b6154091ff009
Morgan Martinet <morgan.martinet@ville.montreal.qc.ca>
2017-09-21 00:33:21 -0400

    add more metrics and refactor code

--- main.go
+++ main.go
@@ -1,39 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"io/ioutil"
-	"encoding/json"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-type MetricCount struct {  
-    Count int `json:"count"`
+type MetricCount struct {
+	Count int `json:"count"`
 }
 
 type Metric struct {
-    Timestamp 	string `json:"timestamp"`
-    Name 		string `json:"name"`
-    Reporter 	string `json:"reporter"`
-    Value 		int `json:"value"`
+	Timestamp string `json:"timestamp"`
+	Name      string `json:"name"`
+	Reporter  string `json:"reporter"`
+	Value     int    `json:"value"`
+}
+
+type ProcessDefinition struct {
+	Id           string `json:"id"`
+	Key          string `json:"key"`
+	Category     string `json:"category"`
+	Description  string `json:"description"`
+	Name         string `json:"name"`
+	Version      int    `json:"version"`
+	Resource     string `json:"resource"`
+	DeploymentId string `json:"deploymentId"`
+	TenantId     string `json:"tenantId"`
+	VersionTag   string `json:"versionTag"`
+}
+
+func (def ProcessDefinition) String() string {
+	if def.Key == "" {
+		return "<EmptyDefinition>"
+	}
+	return fmt.Sprintf("%s@%d", def.Key, def.Version)
+}
+
+type ProcessDefinitionStatisticsResult struct {
+	Id         string            `json:"id"`
+	Instances  int               `json:"instances"`
+	FailedJobs int               `json:"failedJobs"`
+	Definition ProcessDefinition `json:"definition"`
 }
 
 var (
 	serverUrl string
-	verbose bool
-	
-	httpClient = http.Client {
-		Timeout: time.Second * 5, 
+	verbose   bool
+
+	httpClient = http.Client{
+		Timeout: time.Second * 5,
 	}
-	
+
 	incidentsCounter = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "camunda_incidents_total",
@@ -50,6 +78,30 @@ var (
 		[]string{"name"},
 	)
 
+	processInstancesCounter = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "camunda_process_instances_total",
+			Help: "Number of instances of a specific Process",
+		},
+		[]string{"id", "definitionId", "definitionKey", "definitionVersion", "deploymentId", "tenantId"},
+	)
+
+	processFailedJobsCounter = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "camunda_process_failed_jobs_total",
+			Help: "Number of failed jobs for a specific Process",
+		},
+		[]string{"id", "definitionId", "definitionKey", "definitionVersion", "deploymentId", "tenantId"},
+	)
+
+	processActivitiesCounter = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "camunda_process_activities_total",
+			Help: "Number of instances of a specific activity",
+		},
+		[]string{"activityId", "activityName", "definitionKey"},
+	)
+
 	errorCounter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "camunda_scrape_errors_total",
@@ -59,46 +111,47 @@ var (
 	)
 )
 
-func fetchCount(api string) (int, error) {
-	url := serverUrl + api
+func fetchJson(url string, data interface{}) error {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
-    if err != nil {
-		return 0, err
-    }
+	if err != nil {
+		return err
+	}
 	res, getErr := httpClient.Do(req)
-    if getErr != nil {
-        return 0, getErr
-    }
-
-    body, readErr := ioutil.ReadAll(res.Body)
-    if readErr != nil {
-        return 0, readErr
-    }
-
-	metric := MetricCount {}
-    jsonErr := json.Unmarshal(body, &metric)
-    if jsonErr != nil {
-        return 0, jsonErr
+	if getErr != nil {
+		return getErr
+	}
+	if res.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("%s => %d", url, res.StatusCode))
+	}
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		return readErr
 	}
 
-	return metric.Count, nil
+	jsonErr := json.Unmarshal(body, data)
+	if jsonErr != nil {
+		return jsonErr
+	}
+	return nil
 }
 
 func fetchIncidents(status string) (int, error) {
-	count, err := fetchCount(fmt.Sprintf("/engine-rest/history/incident/count?%s=true", status))
+	url := fmt.Sprintf("%s/engine-rest/history/incident/count?%s=true", serverUrl, status)
+	metric := MetricCount{}
+	err := fetchJson(url, &metric)
 	if err != nil {
 		log.Printf("Could not fetch count of %s incidents: %s\n", status, err)
 		errorCounter.With(prometheus.Labels{"name": "incidents"}).Inc()
 		return 0, err
 	}
 	if verbose {
-		log.Printf("%d %s incidents\n", count, status)
+		log.Printf("%d %s incidents\n", metric.Count, status)
 	}
-	incidentsCounter.With(prometheus.Labels{"status": status}).Set(float64(count))
-	return count, nil
+	incidentsCounter.With(prometheus.Labels{"status": status}).Set(float64(metric.Count))
+	return metric.Count, nil
 }
 
-func fetchMultipleIncidents(statuses []string) error {
+func fetchMultipleIncidents(statuses ...string) error {
 	var hasErrors = false
 	for _, status := range statuses {
 		if _, err := fetchIncidents(status); err != nil {
@@ -117,29 +170,15 @@ func fetchMetrics(maxResults int, startDate string) ([]Metric, error) {
 		url += fmt.Sprintf("&startDate=%s", startDate)
 	}
 
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-    if err != nil {
+	metrics := []Metric{}
+	err := fetchJson(url, &metrics)
+	if err != nil {
 		return nil, err
-    }
-	res, getErr := httpClient.Do(req)
-    if getErr != nil {
-        return nil, getErr
-    }
-
-    body, readErr := ioutil.ReadAll(res.Body)
-    if readErr != nil {
-        return nil, readErr
-    }
-
-	metrics := []Metric {}
-    jsonErr := json.Unmarshal(body, &metrics)
-    if jsonErr != nil {
-        return nil, jsonErr
 	}
 	return metrics, nil
 }
 
-func fetchLatestMetrics() ([]Metric, error) {
+func collectMetrics() ([]Metric, error) {
 	// first, get only one metric in order to grab the timestamp
 	metrics, err := fetchMetrics(1, "")
 	if err != nil {
@@ -160,26 +199,137 @@ func fetchLatestMetrics() ([]Metric, error) {
 	}
 	for _, metric := range metrics {
 		metricsCounter.With(prometheus.Labels{"name": metric.Name}).Set(float64(metric.Value))
-		log.Printf("metric %s %s = %d\n", metric.Timestamp, metric.Name, metric.Value)
+		if verbose {
+			log.Printf("metric %s %s = %d\n", metric.Timestamp, metric.Name, metric.Value)
+		}
 	}
 	return metrics, nil
 }
 
-func fetchForShort() error {
-	statuses := []string {"open", "closed", "resolved"}
-	err := fetchMultipleIncidents(statuses)
+func fetchProcessDefinitionStatistics() ([]ProcessDefinitionStatisticsResult, error) {
+	url := fmt.Sprintf("%s/engine-rest/process-definition/statistics?failedJobs=true", serverUrl)
+	result := []ProcessDefinitionStatisticsResult{}
+	err := fetchJson(url, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func createCounterLabelsFromStats(stat ProcessDefinitionStatisticsResult) prometheus.Labels {
+	return prometheus.Labels{
+		"id":                stat.Id,
+		"definitionId":      stat.Definition.Id,
+		"definitionKey":     stat.Definition.Key,
+		"definitionVersion": strconv.Itoa(stat.Definition.Version),
+		"deploymentId":      stat.Definition.DeploymentId,
+		"tenantId":          stat.Definition.TenantId}
+}
+
+func collectProcessDefinitionStatistics() error {
+	stats, err := fetchProcessDefinitionStatistics()
+	if err != nil {
+		errorCounter.With(prometheus.Labels{"name": "processDefinitionStatistics"}).Inc()
+		return err
+	}
+	if verbose {
+		log.Printf("Found %d process definition stats\n", len(stats))
+	}
+	for _, stat := range stats {
+		labels := createCounterLabelsFromStats(stat)
+		processInstancesCounter.With(labels).Set(float64(stat.Instances))
+		processFailedJobsCounter.With(labels).Set(float64(stat.FailedJobs))
+		if verbose {
+			//log.Printf("%s: %d instances / %d failedJobs\n", stat.Definition, stat.Instances, stat.FailedJobs)
+		}
+	}
+	return nil
+}
+
+func fetchActivityInstanceCount(activityId string, activityName string, definitionKey string) (int, error) {
+	url := fmt.Sprintf("%s/engine-rest/history/activity-instance/count?activityId=%s", serverUrl, activityId)
+	metric := MetricCount{}
+	err := fetchJson(url, &metric)
+	if err != nil {
+		return 0, err
+	}
+	return metric.Count, nil
+}
+
+func collectActivityInstanceCount(activityId string, activityName string, definitionKey string) (int, error) {
+	count, err := fetchActivityInstanceCount(activityId, activityName, definitionKey)
+	if err != nil {
+		log.Printf("Could not fetch count of %s activities: %s\n", activityId, err)
+		errorCounter.With(prometheus.Labels{"name": "activities"}).Inc()
+		return 0, err
+	}
+	if verbose {
+		log.Printf("%s (%s) = %d\n", activityName, activityId, count)
+	}
+	processActivitiesCounter.With(prometheus.Labels{"activityId": activityId, 
+		"activityName": activityName, 
+		"definitionKey": definitionKey}).Set(float64(count))
+	return count, nil
+}
+
+func collectActivities() error {
+	_, err := collectActivityInstanceCount("StartEvent_0l8qdec", "Service Requests Created", "ProcessOnlineServiceRequest")
 	if err != nil {
 		return err
 	}
-	return nil	
+	_, err = collectActivityInstanceCount("EndEvent_0tgip1s", "Service Requests Closed", "ProcessOnlineServiceRequest")
+	if err != nil {
+		return err
+	}
+	_, err = collectActivityInstanceCount("ServiceTask_1010dsd", "Authenticated Service Requests", "ProcessOnlineServiceRequest")
+	if err != nil {
+		return err
+	}
+	_, err = collectActivityInstanceCount("EndEvent_154enzk", "Anonymous Service Requests", "ProcessOnlineServiceRequest")
+	if err != nil {
+		return err
+	}
+	_, err = collectActivityInstanceCount("EndEvent_1n9otub", "Anonymous Service Requests", "ProcessOnlineServiceRequest")
+	if err != nil {
+		return err
+	}
+	_, err = collectActivityInstanceCount("ServiceTask_0yuo3ru", "Subscriptions created", "ProcessOnlineServiceRequest")
+	if err != nil {
+		return err
+	}
+	_, err = collectActivityInstanceCount("SendTask_1fjvapz", "Send Email", "ProcessOnlineServiceRequest_emailSend")
+	if err != nil {
+		return err
+	}
+	_, err = collectActivityInstanceCount("EndEvent_1uexl7r", "Individuals Requested Not to be notified", "ProcessOnlineServiceRequest_emailSend")
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func fetchForLong() error {
-	_, errMetrics := fetchLatestMetrics()
+func fetchForShortTimer() error {
+	err := fetchMultipleIncidents("open", "deleted", "resolved")
+	if err != nil {
+		return err
+	}
+	err = collectProcessDefinitionStatistics()
+	if err != nil {
+		return err
+	}
+	err = collectActivities()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func fetchForLongTimer() error {
+	_, errMetrics := collectMetrics()
 	if errMetrics != nil {
 		return errMetrics
 	}
-	return nil	
+	return nil
 }
 
 func init() {
@@ -187,6 +337,9 @@ func init() {
 	prometheus.MustRegister(incidentsCounter)
 	prometheus.MustRegister(metricsCounter)
 	prometheus.MustRegister(errorCounter)
+	prometheus.MustRegister(processInstancesCounter)
+	prometheus.MustRegister(processFailedJobsCounter)
+	prometheus.MustRegister(processActivitiesCounter)
 }
 
 func main() {
@@ -207,7 +360,7 @@ func main() {
 	}
 	// Get initial stats
 	log.Println("Fetching initial metrics")
-	if fetchForShort() != nil || fetchForLong() != nil {
+	if fetchForShortTimer() != nil || fetchForLongTimer() != nil {
 		log.Println("Could not fetch all the stats. Exiting now!")
 		os.Exit(2)
 	}
@@ -216,13 +369,13 @@ func main() {
 	shortTicker := time.NewTicker(*shortInterval)
 	go func() {
 		for range shortTicker.C {
-			fetchForShort()
+			fetchForShortTimer()
 		}
 	}()
 	longTicker := time.NewTicker(*longInterval)
 	go func() {
 		for range longTicker.C {
-			fetchForLong()
+			fetchForLongTimer()
 		}
 	}()
 	defer longTicker.Stop()
