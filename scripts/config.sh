7f9068a51e674f936523be2c92e18341397dd223
Morgan Martinet <morgan.martinet@ville.montreal.qc.ca>
2017-10-02 00:37:31 -0400

    add restPrefix to Chart values and use Chart Version as an image tag

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,9 @@
 OUTPUT=./build
 REPO_ORG=livetocode
 REPO_NAME=camunda-prometheus-exporter
+IMG_VERSION=`cat charts/camunda-prometheus-exporter/Chart.yaml |grep version: | awk '{print $2}'`
+
+if [ -z "$IMG_VERSION" ]; then
+    echo "Could not extract Chart version"
+    exit 1
+fi
--- scripts/publish-image.sh
+++ scripts/publish-image.sh
@@ -5,5 +5,6 @@ source $SCRIPT_FOLDER/config.sh
 if [[ "$(docker images -q $REPO_ORG/$REPO_NAME 2> /dev/null)" == "" ]]; then
     echo "Could not find image $REPO_ORG/$REPO_NAME. You must call build-image.sh first!"
 else
+    docker push $REPO_ORG/$REPO_NAME:$IMG_VERSION
     docker push $REPO_ORG/$REPO_NAME:latest
 fi
